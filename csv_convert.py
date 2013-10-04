eaaf9a62767055bfd05dffc0efbe51ff0c6de782
jsdiddon <justin@delaris.com>
2013-10-04 08:17:42 -0700

    Added documentation on excel, updated csv file

--- csv_convert.py
+++ csv_convert.py
@@ -9,7 +9,11 @@ csv_file = r"data.csv"
 # it prevents \x1a, Ctrl-z, from ending the stream prematurely
 # and also stops Python converting to / from different line terminators
 # On other platforms, it has no effect
-in_txt = csv.reader(open(txt_file, "rb"), delimiter = '\n')
+
+""" "rU" opens the targeted text file as a text file
+and allows \n, \r, \r\n to be used as new line characters"""
+
+in_txt = csv.reader(open(txt_file, "rU"), delimiter = ',') 
 out_csv = csv.writer(open(csv_file, 'wb'))
 
 out_csv.writerows(in_txt)
